8ec3eacc0f5e639375c1d44f7541e435a161007c
Maxim Ivanov <maximsmthivanov@gmail.com>
2019-11-14 15:22:53 +0300

    changed to HashRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import App from '../containers/App';
+import React from 'react'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import App from '../containers/App'
 
-export const history = createBrowserHistory();
+export const history = createBrowserHistory()
 
 function Routes() {
-	return (
-		<Router history={history}>
-			<Switch>
-				<Route path="/" component={App} />
-			</Switch>
-		</Router>
-	);
+  return (
+    <HashRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </HashRouter>
+  )
 }
 
-export default Routes;
+export default Routes
