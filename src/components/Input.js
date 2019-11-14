8fd65e7d0f32071b8199d756c7da34d25c9fabc7
Maxim Ivanov <maximsmthivanov@gmail.com>
2019-11-15 00:54:56 +0300

    fixed styles

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,23 +1,31 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable global-require */
-import React from 'react';
-import PropTypes from 'prop-types';
-import inputStyles from '../styles/inputStyles.module.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import inputStyles from '../styles/inputStyles.module.css'
 
 export default function Input(props) {
 	return (
 		<div className={inputStyles.container}>
 			<div className={inputStyles.attach}>
-				<img style={{ height: '35px', transform: 'rotate(90deg)' }} src={require('../assets/attach.png')}
-					alt="Attachment button" />
+				<img
+					style={{ height: '5vh', transform: 'rotate(90deg)' }}
+					src={require('../assets/attach.png')}
+					alt="Attachment button"
+				/>
 			</div>
-			<input className={inputStyles.input} onChange={props.onChange} placeholder='Input something...' type="text"
-				value={props.value} />
+			<input
+				className={inputStyles.input}
+				onChange={props.onChange}
+				placeholder="Input something..."
+				type="text"
+				value={props.value}
+			/>
 		</div>
-	);
+	)
 }
 
 Input.propTypes = {
 	value: PropTypes.string.isRequired,
 	onChange: PropTypes.func.isRequired,
-};
+}
--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -1,44 +1,31 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react';
-import PropTypes from 'prop-types';
-import singleChatStyles from '../styles/singleChatStyles.module.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import singleChatStyles from '../styles/singleChatStyles.module.css'
 
 export default function SingleChat(props) {
-	const statusStyle = [{ display: 'none' }, { display: 'block' }];
+	const statusStyle = [{ display: 'none' }, { display: 'block' }]
 
 	return (
 		<div onClick={props.goToChat.bind(this, props.id)} className={singleChatStyles.container}>
 			<div className={singleChatStyles.avatar}>
-				<img style={{ height: '60px' }} src={props.avatar} alt='Profile' />
+				<img style={{ height: '9vh' }} src={props.avatar} alt="Profile" />
 			</div>
 			<div className={singleChatStyles.content}>
-				<div className={singleChatStyles.name}>
-					{props.name}
-				</div>
-				<div className={singleChatStyles.lastMessage}>
-					{props.lastMessageContent}
-				</div>
+				<div className={singleChatStyles.name}>{props.name}</div>
+				<div className={singleChatStyles.lastMessage}>{props.lastMessageContent}</div>
 			</div>
 			<div className={singleChatStyles.status}>
-				<div className={singleChatStyles.time}>
-					{props.lastMessageTime}
-				</div>
-				<div style={statusStyle[(props.counter > 0 ? 1 : 0)]} className={singleChatStyles.counter}>
+				<div className={singleChatStyles.time}>{props.lastMessageTime}</div>
+				<div style={statusStyle[props.counter > 0 ? 1 : 0]} className={singleChatStyles.counter}>
 					{props.counter}
 				</div>
-				<div style={statusStyle[(props.counter < 0 ? 1 : 0)]} className={singleChatStyles.read}>
-					<div style={{
-						backgroundColor: '#4F6EA3',
-						height: '8px',
-						width: '8px',
-						borderRadius: '50%',
-					}} className="read" />
-				</div>
+				<div style={statusStyle[props.counter < 0 ? 1 : 0]} className={singleChatStyles.read} />
 			</div>
 		</div>
-	);
+	)
 }
 
 SingleChat.propTypes = {
@@ -49,8 +36,8 @@ SingleChat.propTypes = {
 	counter: PropTypes.number.isRequired,
 	name: PropTypes.string.isRequired,
 	avatar: PropTypes.string,
-};
+}
 
 SingleChat.defaultProps = {
 	avatar: 'http://obuvasta.ru/wp-content/uploads/2018/12/boy-512.png',
-};
+}
