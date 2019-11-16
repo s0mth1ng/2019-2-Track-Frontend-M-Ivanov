35df9f78b262518056482bba23bb9e93e77f6929
Maxim Ivanov <maximsmthivanov@gmail.com>
2019-11-16 17:39:10 +0300

    changed to BrowserRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import Chats from '../containers/Chats'
 import Profile from '../containers/Profile'
