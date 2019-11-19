c3046ff9b0188b2bf41cad4dbdb21700247ec02e
Maxim Ivanov <maximsmthivanov@gmail.com>
2019-11-19 16:52:48 +0300

    fixed all styles via stylelint

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  printWidth: 80,
-  trailingComma: 'all',
-  singleQuote: true,
-  arrowParens: 'always',
-  jsxBracketSameLine: false,
-  tabWidth: 2,
-  useTabs: false,
+	printWidth: 80,
+	trailingComma: 'all',
+	singleQuote: true,
+	arrowParens: 'always',
+	jsxBracketSameLine: false,
+	tabWidth: 2,
+	useTabs: true,
 };
